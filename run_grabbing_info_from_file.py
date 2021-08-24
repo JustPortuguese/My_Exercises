d00f76862a465271336ff3f9949aff615a8781df
JustPortuguese <84799888+JustPortuguese@users.noreply.github.com>
2021-08-24 10:50:31 +0100

    Create run_grabbing_info_from_file.py

---
+++ run_grabbing_info_from_file.py
@@ -0,0 +1,22 @@
+import re
+import pandas as pd
+
+file_path = input("FILE'S PATH")
+
+create_groups = input('CREATE THE GROUPS TO GRAB THE RIGHT INFO')
+
+group_1, group_2, group_3 = [], [], []
+
+def get_info_from_file():
+    with open(file_path) as file:
+        for line in file:
+            result = re.match(create_groups, line)
+            if result:
+                """
+                group_1.append(result.group(1))
+                group_2.append(result.group(3))
+                group_3.append(result.group(4))
+        return pd.DataFrame([group_1, group_2, group_3]).T
+                """
+        
+get_info_from_file()
